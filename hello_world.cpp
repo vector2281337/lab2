0e8376741d0664513193835cb859da83a91402a2
vtune <vvlukanin13@mail.ru>
2025-06-01 20:11:32 +0000

    Add code comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 
+// Main function that asks for user name
+// and prints greeting message
 int main() {
-  std::string name;
+  std::string name;  // Variable to store user name
   std::cout << "Enter your name: ";
   std::cin >> name;
   std::cout << "Hello world from " << name << std::endl;
