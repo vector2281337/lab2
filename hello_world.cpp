90569fe7b4a0bd36c855647b69294a342d612ba3
vtune <vvlukanin13@mail.ru>
2025-06-01 20:07:16 +0000

    Remove using namespace std and improve style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-using namespace std;
+#include <string>
 
 int main() {
-  string name;
-  cout << "Enter your name: ";
-  cin >> name;
-  cout << "Hello world from " << name << endl;
+  std::string name;
+  std::cout << "Enter your name: ";
+  std::cin >> name;
+  std::cout << "Hello world from " << name << std::endl;
   return 0;
 }
