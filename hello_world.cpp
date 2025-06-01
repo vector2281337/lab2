9cf6c5cc63c21c40435c73faa87e00894072680c
vtune <vvlukanin13@mail.ru>
2025-06-01 20:06:16 +0000

    Add user name input

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World!" << endl;
+  string name;
+  cout << "Enter your name: ";
+  cin >> name;
+  cout << "Hello world from " << name << endl;
   return 0;
 }
