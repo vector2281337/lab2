6ce9c02662b90a0a256da5f7679854245ca52475
vector2281337 <vvlukanin13@mail.ru>
2025-06-01 23:17:19 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 // Main function that asks for user name
-// and prints greeting message
+// and prints greeting message.
 int main() {
-  std::string name;  // Variable to store user name
+  std::string name;  // Variable to store user name.
   std::cout << "Enter your name: ";
   std::cin >> name;
   std::cout << "Hello world from " << name << std::endl;
